4cd3d0699ff0f1ca1dc7a16e16afe02f464845a7
Benedettopana <benedetto.pana@gmail.com>
2024-03-08 16:20:25 +0100

    [add]bundle 03

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -88,11 +88,85 @@ console.log(otherCars);
 /*******************************************************************************
 ​
     Per questo esercizio, rispondi a queste domande:
-    1- Che cosa fa questo codice?
-    2- Sono presenti errori di sintassi?
-    3- Sono presenti errori logici?
+    1- Che cosa fa questo codice? tramite l'array di oggetti filtra le auto tra benzina/diesel/ e tutte le altre tipologie le restituisce.
+    2- Sono presenti errori di sintassi? a  riga59 manca una virgola, riga 67 e 70 va un singolo uguale, è un assegnazione, 67 l'arrow function va scritta al contrario, riga 64 c'è una virgola in più, 
+    3- Sono presenti errori logici? si, riga  74 va messa in and la condizione non in or.
 ​
     Inserisci i tuoi commenti con le risposte alla fine dell'esercizio
-​
+​   
+    const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    }
+];
+
+const gasolineCars = cars.filter( (auto) => auto.type = 'benzina');
+
+const dieselCars = cars.filter( (auto) => {
+    auto.type = 'diesel';
+});
+
+const otherCars = cars.filter( (auto) => {
+    return auto.type !== 'benzina' && auto.type !== 'diesel';
+});
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
+
 *******************************************************************************/
 
